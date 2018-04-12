d633e8f810950cefe18b752cbdfa25a67ee61867
mimikun <dzdzble_Effort_311@outlook.jp>
2018-04-13 01:05:22 +0900

    fix typo

--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,7 @@
 class Student {
     fullName: string;
-    constructor(public firstname: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstname + " " + middleInitial + " " + lastName;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
 }
 
